6cb353f353304fb69805d5d0ca36ddd1a98f4166
rhys-simpson <rhys030102@gmail.com>
2020-10-07 12:36:54 +1000

    Initial code for attributes and methods

--- place.py
+++ place.py
@@ -1,9 +1,41 @@
-"""..."""
-
-
-# Create your Place class in this file
+"""
+Assignment 2 - Place Class
+Rhys Simpson
+"""
 
 
 class Place:
-    """..."""
+    """Represent a Place object"""
+
+    def __init__(self, name, country, priority, visited_status):
+        """Initialise a Place instance.
+
+        name: string, name of place
+        country: string, country of place
+        priority: integer, value for visiting priority
+        visited_status: True/False, marked visited or unvisited
+        """
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.visited_status = visited_status
+
+    def __str__(self):
+        """Return a string of Place object"""
+        return "Name: {} Country: {} Priority: {} Visited Status: {}".format(self.name, self.country, self.priority,
+                                                                             self.visited_status)
+
+    def mark_visited(self):
+        """Mark a place as visited"""
+        return self.visited_status == "*"
+
+    def mark_unvisited(self):
+        """Mark a place as unvisited"""
+        return self.visited_status == ""
+
+    def is_important(self):
+        """Determine if a place is important (important if - priority <= 2)"""
+        if self.priority <= 2:
+            return "Place is important"
+
     pass
