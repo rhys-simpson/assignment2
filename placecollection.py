abc3ea38562cd08997fb79801de39ff886932c7c
rhys-simpson <rhys030102@gmail.com>
2020-10-09 15:05:13 +1000

    changed/ fixed load places and add place methods. added sort method. fixed __str__ method.

--- placecollection.py
+++ placecollection.py
@@ -3,6 +3,7 @@ Assignment 2 - Place Collection Class
 Rhys Simpson
 """
 
+from place import Place
 from operator import attrgetter
 
 
@@ -15,44 +16,42 @@ class PlaceCollection:
 
     def __str__(self):
         """Return a string of PlaceCollection with Place objects"""
-        place = [place_data for place_data in self.places]
-        return "{}".format(place)
+        return str([str(place) for place in self.places])
 
     def load_places(self, filename):
         """Load places from csv and store into places list"""
         in_file = open(filename, "r")
         for line in in_file:
             parts = line.strip().split(",")
-            if parts[3] == "n":
-                parts[3] = False
-            else:
-                parts[3] = True
-            parts[2] = int(parts[2])
-            self.places.append(parts)
+            new_place = Place(name=parts[0], country=parts[1], priority=int(parts[2]), is_visited=parts[3])
+            self.add_place(new_place)
         in_file.close()
         return self.places
+
+    def add_place(self, add_place):
+        """Add a place object to places attribute"""
+        self.places.append(add_place)
+
+    def sort(self, sort_method=""):
+        """Sort places based on key passed in, then by priority"""
+        if sort_method == "name":
+            return self.places.sort(key=attrgetter(sort_method, "priority"))
+        elif sort_method == "country":
+            return self.places.sort(key=attrgetter(sort_method, "priority"))
+        elif sort_method == "is_visited":
+            return self.places.sort(key=attrgetter("is_visited", "priority"))
+        else:
+            return self.places.sort(key=attrgetter("priority"))
 
     # TODO
     def save_places(self):
         """Save places from the places list to the csv file"""
 
-    def add_place(self, add_place):
-        """Add a place object to places attribute"""
-        new_place = str(add_place).split(',')
-        new_place[2] = int(new_place[2])
-        self.places.append(new_place)
-
+    #TODO - fix
     def get_unvisited_places(self):
         """Retrieve number of unvisited places in list"""
         unvisited_places_number = 0
-        for place_data in self.places:
-            if place_data[3] == "n":
+        for places_data in self.places:
+            if places_data[3] is False:
                 unvisited_places_number += 1
         return unvisited_places_number
-
-    # TODO
-    def sort(self):
-        """ """
-        name_priority = {}
-        for place in self.places:
-            name_priority[place[0]] = place[2]
